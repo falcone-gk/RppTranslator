de631de21f1fb2ed3ca11b020a65ab7155e6e5cf
Enzo Falcon Astuhuayhua <falcone134@gmail.com>
2021-02-16 17:21:16 -0500

    Version 1.0

--- app.py
+++ app.py
@@ -1,10 +1,33 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+from rpp_scrapper import rpp_news
+
+data = {"title": "",
+        "date": "",
+        "summary": "",
+        "img_url": "",
+        "body": ""}
+
+is_scraped = False
 
 app = Flask(__name__)
 
-@app.route('/')
+"""@app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template('index.html')"""
+
+@app.route('/', methods=['GET', 'POST'])
+def scraper():
+
+    global data, is_scraped
+
+    if request.method == 'POST':
+        url = request.form['url']
+        data = rpp_news(url)
+        is_scraped = True
+        return redirect("/")
+
+    else:
+        return render_template("index.html", data=data, is_scraped=is_scraped)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- rpp_scrapper.py
+++ rpp_scrapper.py
@@ -1,5 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
+from googletrans import Translator
+
+def __translate_text(text):
+
+    clean_text = text.replace('\xa0', '')
+    translator = Translator()
+    return translator.translate(clean_text).text
 
 def rpp_news(url):
     """
@@ -25,18 +32,19 @@ def rpp_news(url):
     img_url = soup.find('div', class_='cover').find('img')['src']
 
     # Getting news body.
-    body = [p.text for p in soup.find(id='article-body').find_all('p')]
+    body = [__translate_text(p.text) for p in soup.find(id='article-body').find_all('p')]
 
     return {
-        "title": title,
+        "title": __translate_text(title),
         "date": date,
-        "summary": summary,
+        "summary": __translate_text(summary),
         "img_url": img_url,
         "body": body
     }
 
 def main():
     val = rpp_news('https://rpp.pe/politica/gobierno/francisco-sagasti-martin-vizcarra-cambia-de-version-y-pone-en-tela-de-juicio-todo-el-proceso-de-prueba-de-las-vacunas-noticia-1320932')
+    print(val)
 
 if __name__ == '__main__':
     main()
